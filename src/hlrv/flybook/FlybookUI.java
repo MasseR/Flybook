c855800295d24b116ce6da222a9368f8e0f6c749
Janne Virtanen <jakrvir@gmail.com>
2013-02-10 13:11:26 +0200

    Initial Vaadin7 Eclipse project files

---
+++ src/hlrv/flybook/FlybookUI.java
@@ -0,0 +1,17 @@
+package hlrv.flybook;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+/**
+ * Main UI class
+ */
+@SuppressWarnings("serial")
+public class FlybookUI extends UI {
+
+	@Override
+	protected void init(VaadinRequest request) {
+
+	}
+
+}
