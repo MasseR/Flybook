4723534cbf8c12c72bdac0d8c17e080c29334ec7
Mats Rauhala <mats.rauhala@gmail.com>
2013-02-18 10:17:13 +0200

    Hash classes

---
+++ src/hlrv/flybook/auth/Hash.java
@@ -0,0 +1,30 @@
+package hlrv.flybook.auth;
+
+import hlrv.flybook.auth.BCrypt;
+
+public class Hash
+{
+    protected String hashString;
+
+    public Hash()
+    {
+    }
+
+    public Hash(String existingHash)
+    {
+        this.hashString = existingHash;
+    }
+
+    public static Hash hash(String str)
+    {
+        Hash hash = new Hash();
+        hash.hashString = BCrypt.hashpw(str, BCrypt.gensalt());
+
+        return hash;
+    }
+
+    public boolean check(String str)
+    {
+        return BCrypt.checkpw(str, this.hashString);
+    }
+}
