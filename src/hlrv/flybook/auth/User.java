72bb8b34eaf414bd2884602c80318c81572503db
Mats Rauhala <mats.rauhala@gmail.com>
2013-02-19 21:51:17 +0200

    Getters and setters

--- src/hlrv/flybook/auth/User.java
+++ src/hlrv/flybook/auth/User.java
@@ -3,4 +3,19 @@ package hlrv.flybook.auth;
 public class User
 {
     private String username;
+
+    public User(String username)
+    {
+        this.username = username;
+    }
+
+    public String getUsername()
+    {
+        return this.username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
 }
