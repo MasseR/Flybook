ea78521b669c33fcab12f1261685ca1a83f0bd47
Janne Virtanen <jakrvir@gmail.com>
2013-02-25 22:04:14 +0200

    Change "hash" to "passwd"

--- src/hlrv/flybook/auth/Auth.java
+++ src/hlrv/flybook/auth/Auth.java
@@ -1,53 +1,55 @@
 package hlrv.flybook.auth;
 
-import hlrv.flybook.auth.User;
 import hlrv.flybook.SessionContext;
-import hlrv.flybook.auth.Hash;
+
+import java.sql.SQLException;
+
+import com.vaadin.data.Item;
+import com.vaadin.data.util.filter.Compare.Equal;
+import com.vaadin.data.util.sqlcontainer.SQLContainer;
 import com.vaadin.data.util.sqlcontainer.connection.JDBCConnectionPool;
 import com.vaadin.data.util.sqlcontainer.query.TableQuery;
-import com.vaadin.data.util.sqlcontainer.SQLContainer;
-import com.vaadin.data.util.filter.Compare.*;
-import com.vaadin.data.Item;
-import java.sql.SQLException;
 
 // Some design notes. I'm taking a simpler approach even though I increase
 // coupling. I believe we can be fairly certain in this excercise that we won't
 // be needing to change the authentication method, hashing method or database
 // method so I'm hardcoding the behaviour here.
 
-public class Auth
-{
+public class Auth {
     private JDBCConnectionPool dbPool;
     private SQLContainer container;
 
-    public Auth(JDBCConnectionPool dbPool) throws SQLException
-    {
+    public Auth(JDBCConnectionPool dbPool) throws SQLException {
         this.dbPool = dbPool;
         TableQuery tq = new TableQuery("users", dbPool);
         tq.setVersionColumn("optlock");
         this.container = new SQLContainer(tq);
     }
 
-    /** Try to login the user
-     *
-     * Tries to login the user with the given credentials. If the user could
-     * not be logged in, an exception is thrown. Note that I'm currently
-     * throwing general exception as I can't be bothered with finding /
-     * creating a proper exception at this point in time
+    /**
+     * Try to login the user
+     * 
+     * Tries to login the user with the given credentials. If the user could not
+     * be logged in, an exception is thrown. Note that I'm currently throwing
+     * general exception as I can't be bothered with finding / creating a proper
+     * exception at this point in time
      */
-    public User login(String username, String password, SessionContext context) throws Exception
-    {
+    public User login(String username, String password, SessionContext context)
+            throws Exception {
         this.container.addContainerFilter(new Equal("username", username));
         Object id = this.container.firstItemId();
         Item item = this.container.getItem(id);
-        if(id == null)
+        if (id == null) {
             throw new Exception("User not found");
-        Hash hash = new Hash((String)item.getItemProperty("hash").getValue());
-        if(hash.check(password)) {
-            String firstname = (String)item.getItemProperty("firstname").getValue();
-            String lastname = (String)item.getItemProperty("lastname").getValue();
-            String email = (String)item.getItemProperty("email").getValue();
-            boolean admin = (Boolean)item.getItemProperty("admin").getValue();
+        }
+        Hash hash = new Hash((String) item.getItemProperty("passwd").getValue());
+        if (hash.check(password)) {
+            String firstname = (String) item.getItemProperty("firstname")
+                    .getValue();
+            String lastname = (String) item.getItemProperty("lastname")
+                    .getValue();
+            String email = (String) item.getItemProperty("email").getValue();
+            boolean admin = (Boolean) item.getItemProperty("admin").getValue();
             User user = new User(username, firstname, lastname, email, admin);
             // XXX: Esa, set the session here
 
@@ -57,15 +59,12 @@ public class Auth
         throw new Exception("Password incorrect");
     }
 
-    public void logout()
-    {
+    public void logout() {
     }
 
-    public void getUser()
-    {
+    public void getUser() {
     }
 
-    public void register(String username, String password)
-    {
+    public void register(String username, String password) {
     }
 }
