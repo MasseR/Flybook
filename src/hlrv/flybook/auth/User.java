f39624dc4190be11e071a8ca084192de18c5d084
Mats Rauhala <mats.rauhala@gmail.com>
2013-02-25 15:55:12 +0200

    More fields to users

--- src/hlrv/flybook/auth/User.java
+++ src/hlrv/flybook/auth/User.java
@@ -3,10 +3,14 @@ package hlrv.flybook.auth;
 public class User
 {
     private String username;
+    private String email;
+    private boolean admin;
 
-    public User(String username)
+    public User(String username, String email, boolean admin)
     {
         this.username = username;
+        this.email    = email;
+        this.admin    = adin;
     }
 
     public String getUsername()
@@ -17,5 +21,25 @@ public class User
     public void setUsername(String username)
     {
         this.username = username;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getEmail()
+    {
+        return this.email;
+    }
+
+    public boolean isAdmin()
+    {
+        return this.admin;
+    }
+
+    public void setAdmin(boolean isAdmin)
+    {
+        this.admin = isAdmin;
     }
 }
