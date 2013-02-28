e1762f4b0f6b517e81836dc10551b2483d5c3b28
Janne Virtanen <jakrvir@gmail.com>
2013-02-28 02:46:30 +0200

    Change "password" to "passwd"

--- src/hlrv/flybook/managers/UserManager.java
+++ src/hlrv/flybook/managers/UserManager.java
@@ -1,64 +1,60 @@
 package hlrv.flybook.managers;
 
+import hlrv.flybook.auth.Hash;
+import hlrv.flybook.auth.User;
+
+import java.sql.SQLException;
+
+import com.vaadin.data.Item;
 import com.vaadin.data.util.filter.Compare.Equal;
 import com.vaadin.data.util.sqlcontainer.SQLContainer;
 import com.vaadin.data.util.sqlcontainer.connection.JDBCConnectionPool;
 import com.vaadin.data.util.sqlcontainer.query.TableQuery;
-import com.vaadin.data.Item;
-import java.sql.SQLException;
-import hlrv.flybook.auth.User;
-import hlrv.flybook.auth.Hash;
 
-public class UserManager
-{
+public class UserManager {
     private SQLContainer container;
 
-    public UserManager(JDBCConnectionPool pool) throws SQLException
-    {
+    public UserManager(JDBCConnectionPool pool) throws SQLException {
         TableQuery tq = new TableQuery("users", pool);
         tq.setVersionColumn("optlock");
         this.container = new SQLContainer(tq);
     }
 
-    private Item getItemFromUsername(String username) throws Exception
-    {
+    private Item getItemFromUsername(String username) throws Exception {
         this.container.removeAllContainerFilters();
         this.container.addContainerFilter(new Equal("username", username));
         Object id = this.container.firstItemId();
-        if(id == null) {
+        if (id == null) {
             throw new Exception("User not found");
         }
         return this.container.getItem(id);
     }
 
-    public User getFromUsername(String username) throws Exception
-    {
-        Item item        = this.getItemFromUsername(username);
-        String firstname = (String) item.getItemProperty("firstname").getValue();
-        String lastname  = (String) item.getItemProperty("lastname").getValue();
-        String email     = (String) item.getItemProperty("email").getValue();
+    public User getFromUsername(String username) throws Exception {
+        Item item = this.getItemFromUsername(username);
+        String firstname = (String) item.getItemProperty("firstname")
+                .getValue();
+        String lastname = (String) item.getItemProperty("lastname").getValue();
+        String email = (String) item.getItemProperty("email").getValue();
         return new User(username, firstname, lastname, email, false);
     }
 
-    public String getHashCode(String username) throws Exception
-    {
+    public String getHashCode(String username) throws Exception {
         Item item = this.getItemFromUsername(username);
         return (String) item.getItemProperty("password").getValue();
     }
 
-    public void createUser(User user, Hash password) throws Exception
-    {
-        Item newUser = (Item)this.container.addItem();
+    public void createUser(User user, Hash password) throws Exception {
+        Item newUser = (Item) this.container.addItem();
         newUser.getItemProperty("username").setValue(user.getUsername());
         newUser.getItemProperty("firstname").setValue(user.getFirstname());
         newUser.getItemProperty("lastname").setValue(user.getLastname());
         newUser.getItemProperty("email").setValue(user.getEmail());
-        newUser.getItemProperty("password").setValue(password.raw());
+        newUser.getItemProperty("passwd").setValue(password.raw());
         this.container.commit();
     }
 
-    public void modifyUser(User user) throws Exception
-    {
+    public void modifyUser(User user) throws Exception {
         Item item = this.getItemFromUsername(user.getUsername());
         item.getItemProperty("username").setValue(user.getUsername());
         item.getItemProperty("firstname").setValue(user.getUsername());
@@ -67,14 +63,13 @@ public class UserManager
         this.container.commit();
     }
 
-    public void modifyUser(User user, Hash hash) throws Exception
-    {
+    public void modifyUser(User user, Hash hash) throws Exception {
         Item item = this.getItemFromUsername(user.getUsername());
         item.getItemProperty("username").setValue(user.getUsername());
         item.getItemProperty("firstname").setValue(user.getUsername());
         item.getItemProperty("lastname").setValue(user.getUsername());
         item.getItemProperty("email").setValue(user.getUsername());
-        item.getItemProperty("password").setValue(user.getUsername());
+        item.getItemProperty("passwd").setValue(user.getUsername());
         this.container.commit();
     }
 }
